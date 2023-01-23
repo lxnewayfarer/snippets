a8db5d390748839e3d66509ce9d02be6270373ef
Andrey Dolgikh <lxnewayfarer@yandex.ru>
2023-01-23 22:58:06 +0700

    update

--- calling_classes_from_string.rb
+++ calling_classes_from_string.rb
@@ -1,6 +1,8 @@
 # Get some user's state (e.g. "main_menu") and convert it to a class name (camelize and inflect)
-state = StateMachine.current_state.classify
+state = StateMachine.current_state.camelize
+
 # Search declared handler constant (class)
-handler = "StateInteractions::#{state}::Handle".constantize
+interaction_class = "StateInteractions::#{state}::Handle".constantize
+
 # Run handler
-handler.run!(user: user)
+interaction_class.run!(user: user)
